a0b0beee6fb814a65dd03d87af6cdc73f414418e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:11:15 +0530

    const ticketSchema = new Schema({ /* schema definition */ }, { timestamps: true });: This defines the schema for the "tickets" collection in the MongoDB database. The schema specifies the structure of documents (or records) stored in the collection. Inside the schema definition: title, description, category, status: These fields are defined as String type, indicating they store textual data. priority, progress: These fields are defined as Number type, suggesting they store numeric data. active: This field is defined as Boolean type, indicating it stores boolean (true/false) values.

--- app/models/Ticket.js
+++ app/models/Ticket.js
@@ -4,7 +4,15 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 
 const ticketSchema = new Schema(
-  
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  }
 );
 
 
