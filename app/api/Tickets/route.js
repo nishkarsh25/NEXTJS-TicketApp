568f4e9898489cd2a834064faf6be8e43fba3a2c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:05:45 +0530

    GET Function:
    This function is an asynchronous function that handles HTTP GET requests.
    It tries to fetch all tickets from the database using the Ticket.find() method. This operation is assumed to be asynchronous, hence the await keyword.
    If the operation is successful, it returns a JSON response containing the fetched tickets with a status code of 200 (OK).
    If an error occurs during the operation, it catches the error, logs it to the console, and returns a JSON response with an error message and status code 500 (Internal Server Error).

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,6 +1,15 @@
 import Ticket from "@/app/models/Ticket";
 import { NextResponse } from "next/server";
 
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
 
+    return NextResponse.json({ tickets }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+}
 
 
