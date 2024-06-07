6ebd90acb9225221d2d112fe7e8bf53f9e5c2ca8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:46:10 +0530

    State Initialization:
    Initializes the EDITMODE constant based on whether the ticket ID is "new".
    Initializes the router constant using the useRouter hook.
    Initializes the startingTicketData object with default values for form fields.

--- app/(components)/EditTicketForm.jsx
+++ app/(components)/EditTicketForm.jsx
@@ -3,7 +3,16 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const EditTicketForm = ({ ticket }) => {
-  
+  const EDITMODE = ticket._id === "new" ? false : true;
+  const router = useRouter();
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "Hardware Problem",
+  };
 
   
 
