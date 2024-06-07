8cd73bfbd750932d2e0b91f9e9fe1a2c684b5823
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 18:06:07 +0530

    Import Statements:
    The code imports necessary modules and functions:
    faX from @fortawesome/free-solid-svg-icons: This is the icon representing a delete action (usually an 'X' or close icon).
    FontAwesomeIcon from @fortawesome/react-fontawesome: This is the component used to render FontAwesome icons.
    useRouter from next/navigation: This hook is used for navigation and refreshing the page after deletion.

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,6 +1,8 @@
 "use client";
 
-
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 
 
 
