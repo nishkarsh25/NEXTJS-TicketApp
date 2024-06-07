4ff589d82e9315b3eaa41151dc3b7e3e3e2ac63e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:42:55 +0530

    getTicketById Function:
    This is an asynchronous function named getTicketById.
    It takes an id parameter, which represents the ID of the ticket to fetch.
    Inside the function:
    It makes a fetch request to the specified URL (http://localhost:3000/api/Tickets/${id}) to fetch the ticket data.
    It sets cache: "no-store" in the fetch options to ensure that the response is not cached by the browser.
    If the fetch operation is successful (res.ok), it returns the JSON response containing the ticket data.
    If an error occurs during the fetch operation, it catches the error, logs it to the console, and returns undefined.

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,6 +1,20 @@
 import EditTicketForm from "@/app/(components)/EditTicketForm";
 
+const getTicketById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      cache: "no-store",
+    });
 
+    if (!res.ok) {
+      throw new Error("Failed to fetch topic");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 
 
