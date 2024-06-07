650bad75414600998442c38425abbda63a7f596d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:02:31 +0530

    Navigation Links:
    Inside the navigation bar, there are two navigation links represented by Font Awesome icons wrapped in Link components.
    Each Link component points to a different route ("/" and "/TicketPage/new") in the Next.js application.
    The icons (faHome and faTicket) are displayed using the FontAwesomeIcon component, and their respective classes are assigned for styling.
    User Information:
    
    Next to the navigation links, there's a <div> containing the user's email address (jake.lower17@gmail.com). This information is likely to be dynamically populated based on the logged-in user.
    The email address is styled using the class text-default-text.

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -3,7 +3,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 const Nav = () => {
-  
+  return (
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center space-x-4">
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className=" text-default-text">jake.lower17@gmail.com</p>
+      </div>
+    </nav>
+  );
 };
 
 
