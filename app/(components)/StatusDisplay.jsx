46b0b2af164988bc0d74969517347143128a4225
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:26:50 +0530

    getColor Function:
    Inside the component, there's a helper function named getColor, which determines the background color of the status label based on the provided status prop.
    It uses a switch statement to check the value of the status prop in a case-insensitive manner.
    Depending on the value of status, it assigns a specific background color class:
    If status is "done", it assigns the class "bg-green-200".
    If status is "started", it assigns the class "bg-yellow-200".
    If status is "not started", it assigns the class "bg-red-200".
    If status doesn't match any of the above cases, it assigns the default class "bg-slate-700".
    The function returns the determined color class.

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,5 +1,23 @@
 const StatusDisplay = ({ status }) => {
-  
+  const getColor = (status) => {
+    let color;
+    switch (status.toLowerCase()) {
+      case "done":
+        color = "bg-green-200";
+        return color;
+
+      case "started":
+        color = "bg-yellow-200";
+        return color;
+
+      case "not started":
+        color = "bg-red-200";
+        return color;
+      default:
+        color = "bg-slate-700";
+    }
+    return color;
+  };
   
 };
 
