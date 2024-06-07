74713b1e0e658f9abbb1541ea97cc4e1ff8b507d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:38:07 +0530

    Formatting Function:
    formatTimestamp(timestamp): This function formats a timestamp into a human-readable date and time string. It utilizes the toLocaleString() method of the Date object with specific formatting options.

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -5,7 +5,21 @@ import ProgressDisplay from "./ProgressDisplay";
 import Link from "next/link";
 
 const TicketCard = ({ ticket }) => {
-  
+  function formatTimestamp(timestamp) {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleString("en-US", options);
+
+    return formattedDate;
+  }
 
   
 
