44ff3f9e29997f5ccfbe3536e83c6da7ba1e4724
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:08:30 +0530

    DELETE Function:
    This function is an asynchronous function that handles HTTP DELETE requests (used for deleting resources).
    It expects two parameters: req (request object) and { params }.
    It extracts the id parameter from the params object.
    It deletes the ticket from the database with the specified _id using the Ticket.findByIdAndDelete() method.
    If the deletion is successful, it returns a JSON response with a success message and status code 200 (OK).
    Error Handling:
    
    Both PUT and DELETE functions employ a try-catch block to handle errors gracefully. If an error occurs during execution, it's caught in the catch block, logged to the console, and an appropriate error response is sent back to the client.
    Overall, this code represents API routes for retrieving, updating, and deleting ticket data in a Next.js application, with error handling to ensure robustness.

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -26,4 +26,14 @@ export async function PUT(req, { params }) {
   }
 }
 
+export async function DELETE(req, { params }) {
+  try {
+    const { id } = params;
 
+    await Ticket.findByIdAndDelete(id);
+    return NextResponse.json({ message: "Ticket Deleted" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
