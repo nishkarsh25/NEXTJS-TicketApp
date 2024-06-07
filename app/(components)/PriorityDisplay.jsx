7d353c499b1ded00bb85085907d78a2b5ab50bbf
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:20:59 +0530

    PriorityDisplay Component:
    This is a functional React component named PriorityDisplay.
    It takes a single prop priority, which represents the priority level of an item.
    Inside the component, there's a div element with the class flex justify-start align-baseline. This sets up a flex container with items aligned along the baseline and justified to start.

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -1,6 +1,8 @@
 import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
+const PriorityDisplay = ({ priority }) => {
+  
+};
 
 
