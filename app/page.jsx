4e850b9d8e62ffbe04b6edc1654e4906e85ae4f0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:54:39 +0530

    Implementation: It calls the getTickets function to fetch the ticket data. It handles the scenario where no tickets are available by checking if data?.tickets is falsy and returns a message saying "No tickets" if true. It extracts the tickets from the fetched data. It generates a list of unique categories from the tickets using a Set and map method. This ensures that each category is listed only once. The component returns a JSX structure that: Wraps the entire content in a <div> with padding to provide spacing around the content. Iterates over each unique category using map. For each category: It creates a section for that category, with a heading displaying the category name. It filters the tickets to include only those that belong to the current category. It maps over the filtered tickets to render a TicketCard component for each ticket, passing the necessary props to the TicketCard.

--- app/page.jsx
+++ app/page.jsx
@@ -18,13 +18,18 @@ const getTickets = async () => {
 };
 
 const Dashboard = async () => {
-  
+  const data = await getTickets();
 
-  
+  // Make sure we have tickets needed for production build.
+  if (!data?.tickets) {
+    return <p>No tickets.</p>;
+  }
 
-  
+  const tickets = data.tickets;
 
-  
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
 
   
 };
