9fec47b5da1362f16e5ff79a629c8b0c8eb7e8fa
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:53:31 +0530

    Fetching Tickets getTickets Function: Purpose: This function fetches the ticket data from an API endpoint. Implementation: It uses the fetch API to make a GET request to a specified URL (http://localhost:3000/api/Tickets). The cache: "no-store" option is specified to ensure that the data fetched is always fresh, bypassing any cached responses. It checks if the response is OK using res.ok. If the response is not OK, it throws an error with a message indicating that the fetching failed. If the response is successful, it parses the JSON data and returns it. If there's an error during the fetching process, it logs an error message to the console.

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import TicketCard from "./(components)/TicketCard";
 
 const getTickets = async () => {
-  
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
 };
 
 
