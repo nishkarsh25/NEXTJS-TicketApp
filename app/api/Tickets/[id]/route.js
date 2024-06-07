6f88bb38c68e0241dd879f0691b0891cafbc5f77
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 17:07:49 +0530

    GET Function:
    This function is an asynchronous function that handles HTTP GET requests.
    It expects two parameters: request and { params }. The params object likely contains route parameters extracted from the URL.
    It extracts the id parameter from the params object.
    It attempts to find a ticket in the database with the specified _id (assuming MongoDB-like _id field) using the Ticket.findOne() method.
    If the ticket is found, it returns a JSON response containing the ticket data with a status code of 200 (OK).

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -1,6 +1,12 @@
 import Ticket from "@/app/models/Ticket";
 import { NextResponse } from "next/server";
 
+export async function GET(request, { params }) {
+  const { id } = params;
+
+  const foundTicket = await Ticket.findOne({ _id: id });
+  return NextResponse.json({ foundTicket }, { status: 200 });
+}
 
 
 
